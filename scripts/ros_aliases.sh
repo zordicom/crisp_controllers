75837e2ad810ae212c21fc4cd61d744739b77964
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-07-29 08:33:47 +0200

    Removed unused docker images

--- scripts/ros_aliases.sh
+++
@@ -1,4 +0,0 @@
-# Some aliases to quickly get compile source and navigate to project
-alias proj="cd ~/ros2_ws/src/crisp_controllers"
-alias cb="cd ~/ros2_ws && colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && proj"
-alias sw="source /opt/ros/$ROS_DISTRO/setup.bash && source ~/ros2_ws/install/setup.bash"
--- scripts/start_dev.sh
+++
@@ -1,3 +0,0 @@
-# Use devcontainer to start a development environment in the terminal
-devcontainer up --workspace-folder . --dotfiles-repository https://github.com/danielsanjosepro/dotfiles.git
-devcontainer exec --workspace-folder . bash
